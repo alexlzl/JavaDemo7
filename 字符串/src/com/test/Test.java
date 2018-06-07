3527b8df0a42d885166de5da5cea470cf2bbf568
liuzhouliang <525688067@qq.com>
2018-06-07 14:52:06 +0800

    q

---
+++ 字符串/src/com/test/Test.java
@@ -0,0 +1,13 @@
+package com.test;
+
+/**
+ * Created by liuzhouliang on 2018/6/7.
+ */
+public class Test {
+    public static void main(String[] args){
+        String str="abcdef";
+        System.out.println(str.codePointAt(1));
+        System.out.println(str.indexOf(98));
+        System.out.println(new StringBuilder().appendCodePoint(98));
+    }
+}
