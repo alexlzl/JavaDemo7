89e681c4ec53617e9ea1b2013d36b784c90000ee
liuzhouliang <525688067@qq.com>
2018-06-07 16:04:51 +0800

    q

---
+++ 输入输出/src/com/test/格式化输出.java
@@ -0,0 +1,11 @@
+package com.test;
+
+/**
+ * Created by liuzhouliang on 2018/6/7.
+ */
+public class 格式化输出 {
+
+    public static void main(String[] args){
+        System.out.println(String.format("%1$s %2$s=====","abc","efg"));
+    }
+}
