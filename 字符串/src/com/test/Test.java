99669c954ee0c717960b8e26439ae78030e78437
liuzhouliang <525688067@qq.com>
2018-06-08 17:46:50 +0800

    q

--- 字符串/src/com/test/Test.java
+++ 字符串/src/com/test/Test.java
@@ -6,8 +6,10 @@ package com.test;
 public class Test {
     public static void main(String[] args){
         String str="abcdef";
+        String name="刘";
         System.out.println(str.codePointAt(1));
         System.out.println(str.indexOf(98));
         System.out.println(new StringBuilder().appendCodePoint(98));
+        System.out.println(name.codePointAt(0));//21016
     }
 }
