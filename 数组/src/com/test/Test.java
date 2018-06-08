85bafa96f1206cc028df6791ea3392c69edc5310
liuzhouliang <525688067@qq.com>
2018-06-08 11:51:15 +0800

    q

---
+++ 数组/src/com/test/Test.java
@@ -0,0 +1,25 @@
+package com.test;
+
+import java.util.Arrays;
+
+/**
+ * Created by liuzhouliang on 2018/6/8.
+ */
+public class Test {
+    public static void main(String[] args){
+        int n=1;
+        int[] a=new int[n];
+        n=100;
+        a[0]=100;
+        System.out.println(a.length);
+        System.out.println(a.toString());
+        System.out.println(Arrays.toString(a));
+
+        int[] aa={1,23,3};
+        int[] bb=new int[]{};
+        int[] cc={};
+
+        System.out.println(Arrays.toString(bb));
+
+    }
+}
