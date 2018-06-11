21c40306c891db1adf70bee6f538c27e8b237ac9
liuzhouliang <525688067@qq.com>
2018-06-11 12:04:27 +0800

    q

--- 字符编码2/src/com/test/Test.java
+++ 字符编码2/src/com/test/Test.java
@@ -17,13 +17,16 @@ public class Test {
     public static void main(String[] args) throws UnsupportedEncodingException {
         String str = "你好hello";
         int byte_len = str.getBytes().length;
-        System.out.println("字节长度为：" + byte_len);//11
+        System.out.println("字节长度为：" + byte_len);//11===因为在utf-8编码中，该中文字符占了3个字节，英文字符占1个字节。
         System.out.println("系统默认编码方式：" + System.getProperty("file.encoding"));
         int byte_len1 = str.getBytes("gbk").length;
-        System.out.println("字节长度为：" + byte_len1);//9
+        System.out.println("字节长度为：" + byte_len1);//9===因为在GBK编码中，中文占2个字节，而英文字符占1个字节。
         System.out.println("系统默认编码方式：" + System.getProperty("file.encoding"));
         int byte_len2 = str.getBytes("utf-16").length;
-        System.out.println("字节长度为：" + byte_len2);//6
+        System.out.println("字节长度为：" + byte_len2);//16=====因为在utf-16编码中，该中文字符占了3个字节，英文字符占2个字节。
+        System.out.println("系统默认编码方式：" + System.getProperty("file.encoding"));
+        int byte_len3 = str.getBytes("utf-32").length;
+        System.out.println("字节长度为：" + byte_len3);//28=====因为在utf-32编码中，所有的字符均占4个字节。
         System.out.println("系统默认编码方式：" + System.getProperty("file.encoding"));
         int len = str.length();
 
