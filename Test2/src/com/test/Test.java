72e1ae27f1d66fb4d5de8befcd020f0cd68e1a43
liuzhouliang <525688067@qq.com>
2018-06-13 10:15:52 +0800

    q

---
+++ Test2/src/com/test/Test.java
@@ -0,0 +1,11 @@
+package com.test;
+
+/**
+ * Created by liuzhouliang on 2018/6/8.
+ */
+public class Test {
+
+    public static void main(String[] args){
+        System.out.println(args[0]);
+    }
+}
--- 字符编码/src/com/test/Test.java
+++ 字符编码/src/com/test/Test.java
@@ -9,6 +9,7 @@ public class Test {
     public static void main(String[] args){
         char a='a';
         char b='严';
+
         System.out.println(Integer.toBinaryString((int)a));//============1100001
         System.out.println(Integer.toBinaryString((int)b));//==========1001 1100 0100 101
         String c="严";
