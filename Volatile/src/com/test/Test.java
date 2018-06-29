0835b833d6cb4f4bbf1d838b5e18129eaa8fc239
liuzhouliang <525688067@qq.com>
2018-06-29 18:54:47 +0800

    q

--- Volatile/src/com/test/Test.java
+++ Volatile/src/com/test/Test.java
@@ -14,14 +14,18 @@ public class Test extends Thread {
 //            i++;
 //            System.out.println("run===" + i);
         }
+        System.out.println("当前线程===" + Thread.currentThread().getName());//当前线程===child thread
         System.out.println("flag===" + flag);
     }
 
     public static void main(String[] args) throws Exception {
         Test vt = new Test();
+        vt.setName("child thread");
         vt.start();
         Thread.sleep(500);
+
         vt.flag = true;
+        System.out.println("当前线程===" + Thread.currentThread().getName());//main
         System.out.println("更改flag================true");
     }
 }
